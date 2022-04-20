e9d550f198cf63c7dcb1f3f7d88362c93c0bed80
VyomPaul <86235294+VyomPaul@users.noreply.github.com>
2022-04-20 17:51:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,8 +23,18 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+
+  video = createCapture(VIDEO);
+  video.hide;
+  video.parent('canvas');
+
+  poseNet = ml5.poseNet(video, modelLoaded)
 }
 
+function modelLoaded() {
+  console.log("Model Loaded!")
+}
 
 function draw(){
 
